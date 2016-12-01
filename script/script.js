47f2cc1596b3ce2a58a8d0ca516ae32d5ca829e0
Burnie <Marylin@CMP014-2.local>
2016-12-01 14:07:35 +0900

    Site changes

--- script/script.js
+++ script/script.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
   $('.menu-box').click(function(){
     $(this).toggleClass('box-active');
-    $('#top').toggleClass('top-line-active')
-    $('#bottom').toggleClass('bottom-line-active')
+    $('#top').toggleClass('top-line-active menu-icon-active')
+    $('#bottom').toggleClass('bottom-line-active menu-icon-active')
     $('.middle-line').toggleClass('middle-line-active')
+    $('.menu-overlay').toggleClass('menu-overlay-active')
+    $('body').toggleClass('body-menu-active')
   });
 });
 
 $(document).scroll(function(){
   var scroller = $(this).scrollTop();
-  $('p').css({
+  $('.container').children().css({
     'transform': 'translateX('+ scroller / 20 +'%)'
   });
 });
