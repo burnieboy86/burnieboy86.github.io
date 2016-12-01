d9ce600b43818732df0d8b6748f57851bbf76b22
Burnie <Marylin@CMP014-2.local>
2016-12-01 15:59:40 +0900

    Add timing delay to menu text

--- script/script.js
+++ script/script.js
@@ -1,11 +1,37 @@
 $(document).ready(function(){
   $('.menu-icon-box').click(function(){
+
     $(this).toggleClass('box-active');
     $('#top').toggleClass('top-line-active menu-icon-active')
     $('#bottom').toggleClass('bottom-line-active menu-icon-active')
     $('.middle-line').toggleClass('middle-line-active')
     $('.menu-wrapper').fadeToggle(200)
     $('body').toggleClass('body-menu-active')
+
+    setTimeout(function(){
+
+      $('#first-item').toggleClass('first-text-active');
+
+    },200);
+
+    setTimeout(function(){
+
+      $('#second-item').toggleClass('second-text-active');
+
+    },400);
+
+    setTimeout(function(){
+
+      $('#third-item').toggleClass('third-text-active');
+
+    },600);
+
+    setTimeout(function(){
+
+      $('#fourth-item').toggleClass('fourth-text-active');
+
+    },800);
+
   });
 });
 
