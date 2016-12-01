1a70357264ad484421b47bd12b19d7b51f594040
Burnie <Marylin@CMP014-2.local>
2016-12-01 10:45:38 +0900

    New site edits

--- script/script.js
+++ script/script.js
@@ -1,11 +1,15 @@
-$(document).scroll(function(){
-
-  var scroller = $(this).scrollTop();
-
-  $('.front, .back').css({
-
-    'transform': 'rotateX('+ scroller / 6 +'deg) translateX('+ scroller / 100 +'%)'
-
+$(document).ready(function(){
+  $('.menu-box').click(function(){
+    $(this).toggleClass('box-active');
+    $('#top').toggleClass('top-line-active')
+    $('#bottom').toggleClass('bottom-line-active')
+    $('.middle-line').toggleClass('middle-line-active')
   });
+});
 
+$(document).scroll(function(){
+  var scroller = $(this).scrollTop();
+  $('p').css({
+    'transform': 'translateX('+ scroller / 20 +'%)'
+  });
 });
