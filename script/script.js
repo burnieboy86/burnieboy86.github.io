6493e82f18e515e10da9625a43cb1d31b9f40b58
Burnie <Marylin@CMP014-2.local>
2016-12-02 14:58:13 +0900

    Add menu text to menu icon

--- script/script.js
+++ script/script.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
+
   $('.menu-icon-box').click(function(){
 
+
     $(this).toggleClass('box-active');
+
     $('#top').toggleClass('top-line-active menu-icon-active')
+
     $('#bottom').toggleClass('bottom-line-active menu-icon-active')
+
     $('.middle-line').toggleClass('middle-line-active')
+
+    $(this).children('p').fadeToggle(200)
+
     $('.menu-wrapper').fadeToggle(200)
+
     $('body').toggleClass('body-menu-active')
+
 
     //setTimeout(function(){
 
@@ -36,11 +46,19 @@ $(document).ready(function(){
 });
 
 $(document).scroll(function(){
+
   var scroller = $(this).scrollTop();
+
   $('.container').children().css({
+
     'transform': 'translateX('+ scroller / 20 +'%)'
+
   });
+
   $('.main-text-character-container').css({
+
     'transform': 'rotateX(-'+ scroller / 20 +'deg) rotateY(-'+ scroller / 20 +'deg)'
+
   })
+
 });
